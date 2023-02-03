d79d6b55dc7d932c43ebccea406284457c5972c1
Pretti78 <pretti768@gmail.com>
2023-02-03 11:54:28 -0300

    v1 prueba

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import './styles/App.css';
+import './style/App.css';
 import { HashRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Purchases from './pages/Purchases';
-import ProductDetail from './pages/ProductDetail';
-import Login from './pages/Login';
 import NavBar from './components/NavBar';
-import LoadingScreen from './components/LoadingScreen';
+import Home from './pages/Inicio';
+import ListaProductos from './pages/ListaProductos';
+import ProductoDetallado from './pages/ProductoDetallado';
+import Compras from './pages/Compras';
+import Devolucion from './pages/Devolucion';
 import { useSelector } from 'react-redux';
-import { Container } from 'react-bootstrap';
-import ProtectedRoutes from './components/ProtectedRoutes';
+import LoadingScreen from './components/LoadingScreen';
 
 function App() {
 	const isLoading = useSelector((state) => state.isLoading);
@@ -17,23 +16,13 @@ function App() {
 		<HashRouter>
 			<NavBar />
 			{isLoading && <LoadingScreen />}
-			<Container className="my-5">
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/products/:id" element={<ProductDetail />} />
-					<Route path="/login" element={<Login />} />
-					<Route element={<ProtectedRoutes />}>
-						<Route path="/purchases" element={<Purchases />} />
-					</Route>
-				</Routes>
-				<footer style={{ textAlign: 'center', fontSize: '1.5rem' }}>
-					<p>page desarrolada al 100% por Pretti Omar</p>
-
-					<a href="https://www.linkedin.com/in/pretti-omar/">Linkedin</a>
-					<br />
-					<a href="https://github.com/Pretti78">Git Hub</a>
-				</footer>
-			</Container>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/productos" element={<ListaProductos />} />
+				<Route path="/productos/:id" element={<ProductoDetallado />} />
+				<Route path="/compras" element={<Compras />} />
+				<Route path="/devolucion" element={<Devolucion />} />
+			</Routes>
 		</HashRouter>
 	);
 }
